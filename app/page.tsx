128d60608e628f7fdfcfce7b3a13f84f6564170f
Sina Shafiee <alisinashafieezada@gmail.com>
2024-04-01 13:34:06 +0330

    fix: typo

--- app/page.tsx
+++ app/page.tsx
@@ -35,7 +35,7 @@ export default function Home() {
       <div className='hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0' />
       <div className='my-16 text-center animate-fade-in'>
         <h2 className='text-sm text-zinc-500 '>
-          Frontend developer crafting seamless user experiences. Let's make
+          Frontend Developer crafting seamless user experiences. Let's make
           visions reality together!
         </h2>
       </div>
